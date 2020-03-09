1961761232d70f5d0a93421c0e48ab3faf08e3d9
hongyu.li <hongyu.li@thoughtworks.com>
2020-03-09 21:03:47 +0800

    [Hongyu]: linear regression original version and gradient descent

---
+++ 02-Regression/regression.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+class LinearRegression:
+    def __init__(self, n_iter=10000, learning_rate=1e-6, gradient_descent=True):
+        self.gradient_descent = gradient_descent
+        self.n_iter = n_iter
+        self.learning_rate = learning_rate
+        self.beta_hat = []
+
+    def fit(self, X, Y):
+        X = np.asarray(X)
+        Y = np.asarray(Y)
+        p = X.shape[1]
+        N = X.shape[0]
+        self.beta_hat = np.zeros(p)
+        if self.gradient_descent:
+            training_loss = []
+            self.beta_hat = np.random.uniform(size=p)
+            for i in range(self.n_iter):
+                residual = Y - X.dot(self.beta_hat)
+                mse = (1 / N) * residual.T.dot(residual)
+                training_loss.append(mse)
+                self.beta_hat -= 2 * self.learning_rate * (-residual.T.dot(X))
+            return training_loss
+        else:
+            self.beta_hat = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
+            residual = Y - X.dot(self.beta_hat)
+            mse = (1 / N) * residual.T.dot(residual)
+            return mse
+
+    def predict(self, X_new):
+        return X_new.dot(self.beta_hat)
