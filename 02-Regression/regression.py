5d6b4e1c60b46bb34eede426e790fcd5e7490fa4
hongyu.li <hongyu.li@thoughtworks.com>
2020-03-10 11:07:58 +0800

    [Hongyu]: add interception option for linear regression

--- 02-Regression/regression.py
+++ 02-Regression/regression.py
@@ -2,18 +2,27 @@ import numpy as np
 
 
 class LinearRegression:
-    def __init__(self, n_iter=10000, learning_rate=1e-6, gradient_descent=True):
+    def __init__(self, n_iter=10000, learning_rate=1e-6, gradient_descent=True, intercept=False):
         self.gradient_descent = gradient_descent
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.beta_hat = []
+        self.intercept = intercept
 
     def fit(self, X, Y):
+
         X = np.asarray(X)
         Y = np.asarray(Y)
         p = X.shape[1]
         N = X.shape[0]
-        self.beta_hat = np.zeros(p)
+
+        if self.intercept:
+            ones = np.reshape(np.ones(N),(N,1))
+            X = np.hstack((ones, X))
+            self.beta_hat = np.zeros(p+1)
+        else:
+            self.beta_hat = np.zeros(p)
+
         if self.gradient_descent:
             training_loss = []
             self.beta_hat = np.random.uniform(size=p)
