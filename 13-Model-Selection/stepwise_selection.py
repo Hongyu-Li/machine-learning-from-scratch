58c1ef9bd494fa71a4eccfc67e5625b66c83668b
hongyu.li <hongyu.li@thoughtworks.com>
2020-07-31 16:19:26 +0800

    [Hongyu]: implement bidirectional selection for linear regression.

--- 13-Model-Selection/stepwise_selection.py
+++ 13-Model-Selection/stepwise_selection.py
@@ -45,7 +45,7 @@ class stepwise_selection:
         if method == 'forward':
             candidate_features = self.__best_features + [var_idx]
         elif method == 'backward':
-            candidate_features = deepcopy(self.__initial_features)
+            candidate_features = deepcopy(self.__best_features)
             candidate_features.remove(var_idx)
         X = self.X[:, candidate_features]
         linear_reg.fit(X, self.y)
@@ -74,6 +74,30 @@ class stepwise_selection:
         y_preds = [linear_reg.predict(x) for x in self.X]
         return calculate_r2(self.y, y_preds)
 
+    def bidirectional_selection(self):
+        for _ in range(self.p):
+            iter_r2_increase = []
+            for var_idx in self.__initial_features:
+                current_r2 = self.__build_model(var_idx, 'forward')
+                iter_r2_increase.append(current_r2 - self.best_r2)
+            iter_r2_increase = np.asarray(iter_r2_increase)
+            if len(iter_r2_increase) > 0 and max(iter_r2_increase) > self.threshold:
+                selected_feature_idx = self.__initial_features.pop(iter_r2_increase.argmax())
+                self.__best_features.append(selected_feature_idx)
+                self.best_r2 += max(iter_r2_increase)
+
+            iter_r2_decrease = []
+            for var_idx in self.__best_features:
+                current_r2 = self.__build_model(var_idx, 'backward')
+                iter_r2_decrease.append(self.best_r2 - current_r2)
+            iter_r2_decrease = np.asarray(iter_r2_decrease)
+            if len(iter_r2_decrease) > 0 and min(iter_r2_decrease) < self.threshold:
+                self.__initial_features.pop(iter_r2_decrease.argmin())
+                self.best_r2 -= min(iter_r2_decrease)
+
+            if max(iter_r2_increase, default=0) < self.threshold < min(iter_r2_decrease,default=0):
+                return self.__feature_names[self.__best_features]
+        return self.__feature_names[self.__best_features]
 
 if __name__ == '__main__':
     data = pd.read_csv('../data/housing.csv', sep=',')
@@ -81,3 +105,5 @@ if __name__ == '__main__':
     a = forward.forward_selection()
     backward = stepwise_selection(data, 0.5)
     b = backward.backward_selection()
+    mix = stepwise_selection(data, 0.01)
+    c = mix.bidirectional_selection()
